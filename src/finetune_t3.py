81557d0b51e2c2fdd2add1a57ee1750486f8f7e0
innokria <rahul7star@gmail.com>
2026-02-07 16:23:14 +1100

    Update finetune_t3.py

--- src/finetune_t3.py
+++ src/finetune_t3.py
@@ -525,9 +525,11 @@ def main():
         raise ValueError("Please provide a HF dataset name or local dataset dir.")
 
     # --- Load Chatterbox model ---
-    chatterbox_model = ChatterboxMultilingualTTS(
-        model_name_or_path=model_args.model_name_or_path
-    )
+    chatterbox_model = ChatterboxMultilingualTTS()
+
+    # chatterbox_model = ChatterboxMultilingualTTS(
+    #     model_name_or_path=model_args.model_name_or_path
+    # )
     t3_model = chatterbox_model.t3
     t3_config = chatterbox_model.t3_cfg
 
